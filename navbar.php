cec6f3be7977ddf308657f3befcff7446ddfdcf7
sadik061 <sadik061@gmail.com>
2018-07-02 18:17:15 +0600

    base

---
+++ navbar.php
@@ -0,0 +1,34 @@
+<div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="py-2" href="#"style="padding-left: 13%;color: white;">
+          <bold>AVEC RABBANI</bold>
+        </a>
+
+      <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08" style="padding-left: 32%;">
+        <ul class="navbar-nav">
+          
+          <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Log In</button>
+            <button type="button" class="btn btn-outline-success my-2 my-sm-0" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Sign In</button>
+          </form>
+          
+        </ul>
+      </div>
+    </nav>
+  </div>
+    <nav class="site-header sticky-top py-1">
+      <div class="container d-flex flex-column flex-md-row justify-content-between">
+        
+        <a class="py-2 d-none d-md-inline-block" href="#">Tour</a>
+        <a class="py-2 d-none d-md-inline-block" href="#">Product</a>
+        <a class="py-2 d-none d-md-inline-block" href="#">Features</a>
+        <a class="py-2 d-none d-md-inline-block" href="#">Enterprise</a>
+        <a class="py-2 d-none d-md-inline-block" href="#">Support</a>
+        <a class="py-2 d-none d-md-inline-block" href="#">Pricing</a>
+        <a class="py-2 d-none d-md-inline-block" href="#">Cart</a>
+      </div>
+    </nav>
