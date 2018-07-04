ba5fd334d8fbac48e156093ad1d2840518add365
arefin070 <arefinislam070@gmail.com>
2018-07-04 20:29:55 +0600

    cousre selec page

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 	<main role="main">
 
       <!-- Main jumbotron for a primary marketing message or call to action -->
+
       <div class="jumbotron">
         <div class="container">
           <h1 class="display-3">Hello, world!</h1>
@@ -20,20 +21,74 @@
         </div>
       </div>
 
+			<div class="container">
+				<h3>Top Courses</h3>
+				<div class="row">
+					<div class="col-sm-3">
+						<div class="card" style="width: 18rem;">
+  						<a href="courses.php"><img class="card-img-top" src="https://image.freepik.com/iconen-gratis/spelen-pijl-naar-rechts-in-een-vierkante-knop_318-53259.jpg" alt="Card image cap"></a>
+  						<div class="card-body">
+    					<h5 class="card-title">Card title</h5>
+    					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  					</div>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="card" style="width: 18rem;">
+  						<a href="courses.php"><img class="card-img-top" src="https://image.freepik.com/iconen-gratis/spelen-pijl-naar-rechts-in-een-vierkante-knop_318-53259.jpg" alt="Card image cap"></a>
+  						<div class="card-body">
+    					<h5 class="card-title">Card title</h5>
+    					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  					</div>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="card" style="width: 18rem;">
+  						<a href="courses.php"><img class="card-img-top" src="https://image.freepik.com/iconen-gratis/spelen-pijl-naar-rechts-in-een-vierkante-knop_318-53259.jpg" alt="Card image cap"></a>
+  						<div class="card-body">
+    					<h5 class="card-title">Card title</h5>
+    					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  					</div>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="card" style="width: 18rem;">
+  						<a href="courses.php"><img class="card-img-top" src="https://image.freepik.com/iconen-gratis/spelen-pijl-naar-rechts-in-een-vierkante-knop_318-53259.jpg" alt="Card image cap"></a>
+  						<div class="card-body">
+    					<h5 class="card-title">Card title</h5>
+    					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  					</div>
+						</div>
+					</div>
+
+
+
+
+				</div>
+
+			</div>
+
       <div class="container">
         <!-- Example row of columns -->
+
+				<h3>Why We Us?</h3>
         <div class="row">
-          <div class="col-md-4">
+          <div class="col-md-3">
             <h2>Heading</h2>
             <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
             <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
           </div>
-          <div class="col-md-4">
+					<div class="col-md-3">
             <h2>Heading</h2>
             <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
             <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
           </div>
-          <div class="col-md-4">
+          <div class="col-md-3">
+            <h2>Heading</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div>
+          <div class="col-md-3">
             <h2>Heading</h2>
             <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
             <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
@@ -43,6 +98,28 @@
         <hr>
 
       </div> <!-- /container -->
+
+			<div class="jumbotron">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6">
+							<h2>Heading</h2>
+							<small>Checkout recently added courses</small>
+							<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+						</div>
+
+						<div class="col-md-6">
+							<div class="card" style="width: 18rem;">
+	  						<a href="courses.php"><img class="card-img-top" src="https://image.freepik.com/iconen-gratis/spelen-pijl-naar-rechts-in-een-vierkante-knop_318-53259.jpg" alt="Card image cap"></a>
+	  						<div class="card-body">
+	    					<h5 class="card-title">Card title</h5>
+	    					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+	  					</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 
     </main>
 
